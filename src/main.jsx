3a3ca21cb511e7d1561fe329e797fd05aee0493b
Parisa Daily <misssweetardor@yahoo.com>
2024-11-21 13:08:56 -0800

    Added router to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,43 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Skills',
+        element: <Skills />,
+      },
+      {
+        path: '/Projects',
+        element: <Projects />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <RouterProvider router={router} />
+);
