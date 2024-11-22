9d6b7a073bee7771bcbda3221a66f9c5b39aef30
Parisa Daily <misssweetardor@yahoo.com>
2024-11-21 22:42:50 -0800

    getting started on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
-        <>
-            Hello world
-        </>
+        <body>
+    <h1 class="text-center"> Certified Junior Developer</h1>
+    <p>Content coming soon!!!!!q</p>
+</body>
     )
 }
