546ad325754a07efaacf3c9ff37e5ed002a3f816
Parisa Daily <misssweetardor@yahoo.com>
2024-11-22 00:03:52 -0800

    Added bio section and centered header text.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,9 @@
 export default function About() {
     return (
         <body>
-    <h1 class="text-center"> Certified Junior Developer</h1>
-    <p>Content coming soon!!!!!q</p>
+    <h1  class="text-center pt-5"> Certified Junior Developer</h1>
+    <p>Hi I am Parisa! Thanks for taking time out of your day to come and check me out, I'm very flattered. I am a junior Fullstack Developer (M.E.R.N) fresh out of school. Attending, University of California, Berkeley's program. I have anut 20 years of working in some form of the technology realm, and I have to admit, I love it. A little about myself, I am a critical thiiker, very diligent, motivated, compssionate, and the list goes on. I've created this application to test out my new skills. Let me know what you think by visiting the "Contact" page in the nav.
+    </p>
 </body>
     )
 }
